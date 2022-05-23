de772d0839f1ff0c6f1bb72ddab74c184568d445
IvanDLar <ivandiaz.lara.m@gmail.com>
2022-05-22 21:03:47 -0500

    Markdown

--- resaltador/lib/resaltador.ex
+++ resaltador/lib/resaltador.ex
@@ -8,8 +8,6 @@ defmodule Resaltador do
   Ivan Diaz Lara A01365801
   """
   def read_file(in_filename, out_filename) do
-    # Single expression of nested calls
-    stream = File.stream!(in_filename)
 
     # Using pipe operator to link the calls
     head = File.read!("template_head.html")
@@ -25,14 +23,15 @@ defmodule Resaltador do
       |> Enum.map(&change_booleans/1)
       |> Enum.map(&change_punctuation_curly_bracket/1)
       |> Enum.map(&change_punctuation_square_bracket/1)
-      # |>Enum.map(&change_NewLines/1)
-      # |>Enum.map(&change_Indentations/1)
+      |> Enum.map(&change_NewLines/1)
+      |> Enum.map(&change_Indentations/1)
 
     File.write(out_filename, [head | text] ++ footer ) #Creates new html file with the text modifications
   end
 
   defp change_object_key(line) do
     #Replace all of the "object keys" for their equivalent in html format
+    #Regex will only mach if there is a : character after the string
     regex = ~r/\"([^\"]*)\"(?=\s*:)/
     Regex.replace(regex, line,
                   "<span class = 'object_key'> \\1 </span>")
@@ -40,6 +39,7 @@ defmodule Resaltador do
 
   defp change_string(line) do
     #Replace all of the "strings" for their equivalent in html format
+    #Regex will only mach if there isn't a : character after the string
     regex = ~r/("(.{1,})"(?!:))/
     Regex.replace(regex, line,
                   "<span class = 'string'> \\1 </span>")
@@ -47,13 +47,15 @@ defmodule Resaltador do
 
   defp change_punctuation(line) do
     #Replace punctuation with the html equivalent
-    regex = ~r/(:|,|;)(?=([^"]*"[^"]*")*[^"]*$)(?!")/ #Regex wont match if any punct is within quotes
+    #Regex wont match if any punct is within quotes
+    regex = ~r/(:|,|;)(?=([^"]*"[^"]*")*[^"]*$)(?!")/
     Regex.replace(regex, line,
                   "<span class = 'punctuation'> \\1 </span>")
   end
 
   defp change_punctuation_curly_bracket(line) do
     #Replace c_brackets with the html equivalent
+    #Regex will match the curly bracket characters
     regex = ~r/({|})/
     Regex.replace(regex, line,
                   "<span class = 'punctuation-curly-bracket'>\\1 </span>")
@@ -61,6 +63,7 @@ defmodule Resaltador do
 
   defp change_punctuation_square_bracket(line) do
     #Replace s_brackets with the html equivalent
+    #Regex will match the square bracket characters
     regex = ~r/(\[|\])/
     Regex.replace(regex, line,
                   "<span class = 'punctuation-square-bracket'> \\1 </span>")
@@ -68,7 +71,8 @@ defmodule Resaltador do
 
   defp change_numbers(line)do
     #Replace numbers with the html equivalent
-    regex = ~r/(([E\-\+\d\.]+)(?=([^"]*"[^"]*")*[^"]*$)(?!"))/ #Regex wont match if any num is within quotes
+    #Regex wont match if any num is within quotes
+    regex = ~r/(([E\-\+\d\.]+)(?=([^"]*"[^"]*")*[^"]*$)(?!"))/
     Regex.replace(regex, line, "<span class = 'numbers'> \\1 </span>")
   end
 
@@ -85,7 +89,7 @@ defmodule Resaltador do
   end
 
   defp change_Indentations(line) do
-    #Replace 2 line indentations
+    #Replace tab indentations with their html counterpart
     regex = ~r/\t{1,}/
     Regex.replace(regex, line, "&nbsp;")
   end
